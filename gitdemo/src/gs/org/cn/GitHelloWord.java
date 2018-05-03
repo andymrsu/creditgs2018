2d040dd0a54fb1cc9f3228afc9b14b5ce08ad8f0
andymrsu <lanzhoumail@163.com>
2018-05-03 11:59:27 +0800

    version 2.0

--- gitdemo/src/gs/org/cn/GitHelloWord.java
+++ gitdemo/src/gs/org/cn/GitHelloWord.java
@@ -5,6 +5,7 @@ public class GitHelloWord {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello word git! 123"); 
+		System.out.println("hello word git! 321"); 
 	}
 
 }
